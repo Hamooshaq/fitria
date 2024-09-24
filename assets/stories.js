f9ac78a1ea27c3ad4efe8daefe72337f72d332e9
Mohammad <mohishaaqq@gmail.com>
2024-09-24 16:07:15 +0700

    Update stories.js

--- assets/stories.js
+++ assets/stories.js
@@ -2,5 +2,5 @@ export default [
   "Love is a flower, you have got to let it grow. – John Lennon.",
   "Love is when imperfection being perfection.",
   "If someone takes responsibility without force, that is love.",
-  "Gatau mau nulis apaa lagi ikaa wkwk"
+  "Problem will loss just follow your flow"
 ];
