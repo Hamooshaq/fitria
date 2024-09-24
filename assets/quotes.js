a898702393e3be3aa827e10e6e30bc4b3fd3de66
Mohammad <mohishaaqq@gmail.com>
2024-09-24 15:54:55 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "Gerix", quotes: "Whatever happens, keep breathing" },
-    { author: "Gerix", quotes: "I Love You 3000 Thousand ♥" },
-    { author: "Gerix", quotes: "Hey! I wuv chuuuu! UwU ♥" },
-    { author: "Gerix", quotes: "I love you." },
-    { author: "Gerix", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "Gerix", quotes: "I love you." },
-    { author: "Gerix", quotes: "I'm sorry for loving you." },
+    { author: "Hamudi", quotes: "Whatever happens, keep breathing" },
+    { author: "Hamudi", quotes: "I Love You 3000 Thousand ♥" },
+    { author: "Hamudi", quotes: "Hey! I proud of you ♥" },
+    { author: "Hamudi", quotes: "I love you." },
+    { author: "Hamudi", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "Hamudi", quotes: "I love you." },
+    { author: "Hamudi", quotes: "I'm happy for loving you." },
 ];
